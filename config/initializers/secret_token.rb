b974e0e8091dadb18fcb878dd16b9002c560c5d8
Mohammad Rashid <dopplesoldner@gmail.com>
2013-03-29 15:23:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Glitter::Application.config.secret_key_base = '6abd27b89570c6d984348ad6323f6224412bc1d10217a7fefcdde99df9f06c673a4af2fc93a32480037fa9700eef98879ace1f5dea9ee513cb9f1ca1ac3b1c82'
